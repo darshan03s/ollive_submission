5f3bbb1fe159e85f456f92e73c3426871cf23d21
Darshan S <darshans2727@gmail.com>
2026-06-23 14:35:48 +0530

    fix(ingest): send proper response for health route

--- ingest/src/router/health.ts
+++ ingest/src/router/health.ts
@@ -3,5 +3,5 @@ import { Router } from 'express'
 export const healthRouter: Router = Router()
 
 healthRouter.get('/', (_req, res) => {
-  res.status(200)
+  res.status(200).json({ status: 'ok' })
 })
